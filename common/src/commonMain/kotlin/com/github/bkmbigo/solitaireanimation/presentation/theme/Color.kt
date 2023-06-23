c148b0d474c2134a4853c3da2b78440ede11a71f
bkmbigo <bkmbigo@gmail.com>
2023-06-23 16:16:56 +0300

    refactor: General refactor

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/theme/Color.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/theme/Color.kt
@@ -3,6 +3,5 @@ package com.github.bkmbigo.solitaireanimation.presentation.theme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 
-
 val lightColorScheme = lightColorScheme()
 val darkColorScheme = darkColorScheme()
--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/theme/utils/FontLoader.kt
+++
@@ -1,4 +0,0 @@
-package com.github.bkmbigo.solitaireanimation.presentation.theme.utils
-
-class FontLoader {
-}
