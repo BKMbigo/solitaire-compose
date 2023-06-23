866bf7c0bb6c6421c279d19c5424b232c1c85323
bkmbigo <bkmbigo@gmail.com>
2023-06-24 00:37:21 +0300

    fix: updated extensions to .xml

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/models/Card.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/models/Card.kt
@@ -11,25 +11,25 @@ enum class Card(
     ACE_OF_SPADES(
         rank = CardRank.ACE,
         suite = CardSuite.SPADE,
-        imageFilename = "card_ace_spade.svg",
-        darkImageFilename = "card_ace_spade_dark.svg"
+        imageFilename = "card_ace_spade.xml",
+        darkImageFilename = "card_ace_spade_dark.xml"
     ),
     ACE_OF_CLOVER(
         rank = CardRank.ACE,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_ace_clover.svg",
-        darkImageFilename = "card_ace_clover_dark.svg"
+        imageFilename = "card_ace_clover.xml",
+        darkImageFilename = "card_ace_clover_dark.xml"
         ),
     ACE_OF_DIAMOND(
         rank = CardRank.ACE,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_ace_diamond.svg",
+        imageFilename = "card_ace_diamond.xml",
         darkImageFilename = null
     ),
     ACE_OF_HEARTS(
         rank = CardRank.ACE,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_ace_hearts.svg",
+        imageFilename = "card_ace_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -37,25 +37,25 @@ enum class Card(
     TWO_OF_SPADES(
         rank = CardRank.TWO,
         suite = CardSuite.SPADE,
-        imageFilename = "card_two_spades.svg",
-        darkImageFilename = "card_two_spades_dark.svg"
+        imageFilename = "card_two_spades.xml",
+        darkImageFilename = "card_two_spades_dark.xml"
     ),
     TWO_OF_CLOVER(
         rank = CardRank.TWO,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_two_clover.svg",
-        darkImageFilename = "card_two_clover_dark.svg"
+        imageFilename = "card_two_clover.xml",
+        darkImageFilename = "card_two_clover_dark.xml"
     ),
     TWO_OF_DIAMOND(
         rank = CardRank.TWO,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_two_diamond.svg",
+        imageFilename = "card_two_diamond.xml",
         darkImageFilename = null
     ),
     TWO_OF_HEARTS(
         rank = CardRank.TWO,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_two_hearts.svg",
+        imageFilename = "card_two_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -63,25 +63,25 @@ enum class Card(
     THREE_OF_SPADES(
         rank = CardRank.THREE,
         suite = CardSuite.SPADE,
-        imageFilename = "card_three_spade.svg",
-        darkImageFilename = "card_three_spade_dark.svg"
+        imageFilename = "card_three_spade.xml",
+        darkImageFilename = "card_three_spade_dark.xml"
     ),
     THREE_OF_CLOVER(
         rank = CardRank.THREE,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_three_clover.svg",
-        darkImageFilename = "card_three_clover_dark.svg"
+        imageFilename = "card_three_clover.xml",
+        darkImageFilename = "card_three_clover_dark.xml"
     ),
     THREE_OF_DIAMOND(
         rank = CardRank.THREE,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_three_diamond.svg",
+        imageFilename = "card_three_diamond.xml",
         darkImageFilename = null
     ),
     THREE_OF_HEARTS(
         rank = CardRank.THREE,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_three_hearts.svg",
+        imageFilename = "card_three_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -89,25 +89,25 @@ enum class Card(
     FOUR_OF_SPADES(
         rank = CardRank.FOUR,
         suite = CardSuite.SPADE,
-        imageFilename = "card_four_spade.svg",
-        darkImageFilename = "card_four_spade_dark.svg"
+        imageFilename = "card_four_spade.xml",
+        darkImageFilename = "card_four_spade_dark.xml"
     ),
     FOUR_OF_CLOVER(
         rank = CardRank.FOUR,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_four_clover.svg",
-        darkImageFilename = "card_four_clover_dark.svg"
+        imageFilename = "card_four_clover.xml",
+        darkImageFilename = "card_four_clover_dark.xml"
     ),
     FOUR_OF_DIAMOND(
         rank = CardRank.FOUR,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_four_diamond.svg",
+        imageFilename = "card_four_diamond.xml",
         darkImageFilename = null
     ),
     FOUR_OF_HEARTS(
         rank = CardRank.FOUR,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_four_hearts.svg",
+        imageFilename = "card_four_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -115,25 +115,25 @@ enum class Card(
     FIVE_OF_SPADES(
         rank = CardRank.FIVE,
         suite = CardSuite.SPADE,
-        imageFilename = "card_five_spade.svg",
-        darkImageFilename = "card_five_spade_dark.svg"
+        imageFilename = "card_five_spade.xml",
+        darkImageFilename = "card_five_spade_dark.xml"
     ),
     FIVE_OF_CLOVER(
         rank = CardRank.FIVE,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_five_clover.svg",
-        darkImageFilename = "card_five_clover_dark.svg"
+        imageFilename = "card_five_clover.xml",
+        darkImageFilename = "card_five_clover_dark.xml"
     ),
     FIVE_OF_DIAMOND(
         rank = CardRank.FIVE,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_five_diamond.svg",
+        imageFilename = "card_five_diamond.xml",
         darkImageFilename = null
     ),
     FIVE_OF_HEARTS(
         rank = CardRank.FIVE,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_five_hearts.svg",
+        imageFilename = "card_five_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -141,25 +141,25 @@ enum class Card(
     SIX_OF_SPADES(
         rank = CardRank.SIX,
         suite = CardSuite.SPADE,
-        imageFilename = "card_six_spade.svg",
-        darkImageFilename = "card_six_spade_dark.svg"
+        imageFilename = "card_six_spade.xml",
+        darkImageFilename = "card_six_spade_dark.xml"
     ),
     SIX_OF_CLOVER(
         rank = CardRank.SIX,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_six_clover.svg",
-        darkImageFilename = "card_six_clover_dark.svg"
+        imageFilename = "card_six_clover.xml",
+        darkImageFilename = "card_six_clover_dark.xml"
     ),
     SIX_OF_DIAMOND(
         rank = CardRank.SIX,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_six_diamond.svg",
+        imageFilename = "card_six_diamond.xml",
         darkImageFilename = null
     ),
     SIX_OF_HEARTS(
         rank = CardRank.SIX,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_six_hearts.svg",
+        imageFilename = "card_six_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -167,25 +167,25 @@ enum class Card(
     SEVEN_OF_SPADES(
         rank = CardRank.SEVEN,
         suite = CardSuite.SPADE,
-        imageFilename = "card_seven_spade.svg",
-        darkImageFilename = "card_seven_spade_dark.svg"
+        imageFilename = "card_seven_spade.xml",
+        darkImageFilename = "card_seven_spade_dark.xml"
     ),
     SEVEN_OF_CLOVER(
         rank = CardRank.SEVEN,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_seven_clover.svg",
-        darkImageFilename = "card_seven_clover_dark.svg"
+        imageFilename = "card_seven_clover.xml",
+        darkImageFilename = "card_seven_clover_dark.xml"
     ),
     SEVEN_OF_DIAMOND(
         rank = CardRank.SEVEN,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_seven_diamond.svg",
+        imageFilename = "card_seven_diamond.xml",
         darkImageFilename = null
     ),
     SEVEN_OF_HEARTS(
         rank = CardRank.SEVEN,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_seven_hearts.svg",
+        imageFilename = "card_seven_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -193,25 +193,25 @@ enum class Card(
     EIGHT_OF_SPADES(
         rank = CardRank.EIGHT,
         suite = CardSuite.SPADE,
-        imageFilename = "card_eight_spade.svg",
-        darkImageFilename = "card_eight_spade_dark.svg"
+        imageFilename = "card_eight_spade.xml",
+        darkImageFilename = "card_eight_spade_dark.xml"
     ),
     EIGHT_OF_CLOVER(
         rank = CardRank.EIGHT,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_eight_clover.svg",
-        darkImageFilename = "card_eight_clover_dark.svg"
+        imageFilename = "card_eight_clover.xml",
+        darkImageFilename = "card_eight_clover_dark.xml"
     ),
     EIGHT_OF_DIAMOND(
         rank = CardRank.EIGHT,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_eight_diamond.svg",
+        imageFilename = "card_eight_diamond.xml",
         darkImageFilename = null
     ),
     EIGHT_OF_HEARTS(
         rank = CardRank.EIGHT,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_eight_hearts.svg",
+        imageFilename = "card_eight_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -219,25 +219,25 @@ enum class Card(
     NINE_OF_SPADES(
         rank = CardRank.NINE,
         suite = CardSuite.SPADE,
-        imageFilename = "card_nine_spade.svg",
-        darkImageFilename = "card_nine_spade_dark.svg"
+        imageFilename = "card_nine_spade.xml",
+        darkImageFilename = "card_nine_spade_dark.xml"
     ),
     NINE_OF_CLOVER(
         rank = CardRank.NINE,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_nine_clover.svg",
-        darkImageFilename = "card_nine_clover_dark.svg"
+        imageFilename = "card_nine_clover.xml",
+        darkImageFilename = "card_nine_clover_dark.xml"
     ),
     NINE_OF_DIAMOND(
         rank = CardRank.NINE,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_nine_diamond.svg",
+        imageFilename = "card_nine_diamond.xml",
         darkImageFilename = null
     ),
     NINE_OF_HEARTS(
         rank = CardRank.NINE,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_nine_hearts.svg",
+        imageFilename = "card_nine_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -245,25 +245,25 @@ enum class Card(
     TEN_OF_SPADES(
         rank = CardRank.TEN,
         suite = CardSuite.SPADE,
-        imageFilename = "card_ten_spade.svg",
-        darkImageFilename = "card_ten_spade_dark.svg"
+        imageFilename = "card_ten_spade.xml",
+        darkImageFilename = "card_ten_spade_dark.xml"
     ),
     TEN_OF_CLOVER(
         rank = CardRank.TEN,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_ten_clover.svg",
-        darkImageFilename = "card_ten_clover_dark.svg"
+        imageFilename = "card_ten_clover.xml",
+        darkImageFilename = "card_ten_clover_dark.xml"
     ),
     TEN_OF_DIAMOND(
         rank = CardRank.TEN,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_ten_diamond.svg",
+        imageFilename = "card_ten_diamond.xml",
         darkImageFilename = null
     ),
     TEN_OF_HEARTS(
         rank = CardRank.TEN,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_ten_hearts.svg",
+        imageFilename = "card_ten_hearts.xml",
         darkImageFilename = null
     ),
 
@@ -271,77 +271,81 @@ enum class Card(
     JUDGE_OF_SPADES(
         rank = CardRank.JUDGE,
         suite = CardSuite.SPADE,
-        imageFilename = "card_judge_spade.svg",
-        darkImageFilename = "card_judge_spade_dark.svg"
+        imageFilename = "card_judge_spade.xml",
+        darkImageFilename = "card_judge_spade_dark.xml"
     ),
     JUDGE_OF_CLOVER(
         rank = CardRank.JUDGE,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_judge_clover.svg",
-        darkImageFilename = "card_judge_spade_dark.svg"
+        imageFilename = "card_judge_clover.xml",
+        darkImageFilename = "card_judge_spade_dark.xml"
     ),
     JUDGE_OF_DIAMOND(
         rank = CardRank.JUDGE,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_judge_diamond.svg",
-        darkImageFilename = "card_judge_diamond_dark.svg"
+        imageFilename = "card_judge_diamond.xml",
+        darkImageFilename = "card_judge_diamond_dark.xml"
     ),
     JUDGE_OF_HEARTS(
         rank = CardRank.JUDGE,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_judge_hearts.svg",
-        darkImageFilename = "card_judge_hearts_dark.svg"
+        imageFilename = "card_judge_hearts.xml",
+        darkImageFilename = "card_judge_hearts_dark.xml"
     ),
 
     // QUEEN
     QUEEN_OF_SPADES(
         rank = CardRank.QUEEN,
         suite = CardSuite.SPADE,
-        imageFilename = "card_queen_spade.svg",
-        darkImageFilename = "card_queen_spade_dark.svg"
+        imageFilename = "card_queen_spade.xml",
+        darkImageFilename = "card_queen_spade_dark.xml"
     ),
     QUEEN_OF_CLOVER(
         rank = CardRank.QUEEN,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_queen_clover.svg",
-        darkImageFilename = "card_queen_clover_dark.svg"
+        imageFilename = "card_queen_clover.xml",
+        darkImageFilename = "card_queen_clover_dark.xml"
     ),
     QUEEN_OF_DIAMOND(
         rank = CardRank.QUEEN,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_queen_diamond.svg",
-        darkImageFilename = "card_queen_diamond_dark.svg"
+        imageFilename = "card_queen_diamond.xml",
+        darkImageFilename = "card_queen_diamond_dark.xml"
     ),
     QUEEN_OF_HEARTS(
         rank = CardRank.QUEEN,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_queen_hearts.svg",
-        darkImageFilename = "card_queen_hearts_dark.svg"
+        imageFilename = "card_queen_hearts.xml",
+        darkImageFilename = "card_queen_hearts_dark.xml"
     ),
 
     // KING
     KING_OF_SPADES(
         rank = CardRank.KING,
         suite = CardSuite.SPADE,
-        imageFilename = "card_king_spade.svg",
-        darkImageFilename = "card_king_spade_dark.svg"
+        imageFilename = "card_king_spade.xml",
+        darkImageFilename = "card_king_spade_dark.xml"
     ),
     KING_OF_CLOVER(
         rank = CardRank.KING,
         suite = CardSuite.CLOVER,
-        imageFilename = "card_king_clover.svg",
-        darkImageFilename = "card_king_clover_dark.svg"
+        imageFilename = "card_king_clover.xml",
+        darkImageFilename = "card_king_clover_dark.xml"
     ),
     KING_OF_DIAMOND(
         rank = CardRank.KING,
         suite = CardSuite.DIAMOND,
-        imageFilename = "card_king_diamond.svg",
-        darkImageFilename = "card_king_diamond_dark.svg"
+        imageFilename = "card_king_diamond.xml",
+        darkImageFilename = "card_king_diamond_dark.xml"
     ),
     KING_OF_HEARTS(
         rank = CardRank.KING,
         suite = CardSuite.HEARTS,
-        imageFilename = "card_king_hearts.svg",
-        darkImageFilename = "card_king_hearts_dark.svg"
+        imageFilename = "card_king_hearts.xml",
+        darkImageFilename = "card_king_hearts_dark.xml"
     );
+    
+    companion object {
+        val cardBackFilename = "card_back.xml"
+    }
 }
