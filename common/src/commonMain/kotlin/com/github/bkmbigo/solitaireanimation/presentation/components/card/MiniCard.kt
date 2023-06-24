2d870308e225616154f98b3c51e13a972f87b486
bkmbigo <bkmbigo@gmail.com>
2023-06-24 12:07:13 +0300

    add: Add Mini-card for low resolution screens

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/components/card/MiniCard.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/components/card/MiniCard.kt
@@ -1,17 +1,31 @@
 package com.github.bkmbigo.solitaireanimation.presentation.components.card
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.bkmbigo.solitaireanimation.models.Card
+import com.github.bkmbigo.solitaireanimation.models.CardColor
 import com.github.bkmbigo.solitaireanimation.presentation.locals.cardtheme.LocalCardTheme
+import com.github.bkmbigo.solitaireanimation.presentation.utils.images.vectorResourceCached
 
 /* [MiniCard] Displays a mini card whenever full cards cannot fit in the screen. Please ensure the card maintain an aspect ratio ~ 155:243
 * */
@@ -22,25 +36,72 @@ fun MiniCard(
     isFlipped: Boolean,
     modifier: Modifier = Modifier
 ) {
-
     val cardTheme = LocalCardTheme.current
 
-   Column(
-       modifier = modifier
+   Surface(
+       modifier = modifier,
+       border = BorderStroke(1.dp, Color.Black),
+       color = cardTheme.cardFrontBackground,
+       shape = RoundedCornerShape(5.dp),
+       shadowElevation = 8.dp
    ) {
-       Row(
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(
-                   horizontal = 4.dp,
-                   vertical = 4.dp
-               ),
-           horizontalArrangement = Arrangement.SpaceBetween,
-           verticalAlignment = Alignment.CenterVertically
-       ) {
-           Text(
-               text = card.rank.symbol,
+       if(isFlipped) {
+           Image(
+               painter = vectorResourceCached(Card.cardBackFilename),
+               contentDescription = null,
+               modifier = Modifier.fillMaxSize()
            )
+       } else {
+           Column(
+               modifier = Modifier.fillMaxSize()
+           ) {
+               Row(
+                   modifier = Modifier
+                       .fillMaxWidth()
+                       .padding(
+                           horizontal = 4.dp
+                       )
+                       .border(1.dp, Color.Black),
+                   horizontalArrangement = Arrangement.SpaceBetween,
+                   verticalAlignment = Alignment.CenterVertically
+               ) {
+                   Text(
+                       text = card.rank.symbol,
+                       fontWeight = FontWeight.Bold,
+                       fontFamily = cardTheme.miniCardFont,
+                       color = when (card.suite.color) {
+                           CardColor.RED -> Card.redColor
+                           CardColor.BLACK -> when (cardTheme.isDark) {
+                               true -> Color.Black
+                               false -> Color.White
+                           }
+                       },
+                       textAlign = TextAlign.Center,
+                       modifier = Modifier.align(Alignment.CenterVertically),
+                       fontSize = 14.sp
+                   )
+
+                   Image(
+                       painter = vectorResourceCached(card.suite.imageFilename),
+                       contentDescription = null,
+                       modifier = Modifier
+                           .size(12.dp)
+                           .align(Alignment.CenterVertically)
+                   )
+               }
+
+               Box(
+                   modifier = Modifier.weight(1f, true).fillMaxWidth()
+               ) {
+                   Image(
+                       painter = vectorResourceCached(card.suite.imageFilename),
+                       contentDescription = null,
+                       modifier = Modifier
+                           .fillMaxSize(0.4f)
+                           .align(Alignment.Center)
+                   )
+               }
+           }
        }
    }
 }
--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/theme/Theme.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/theme/Theme.kt
@@ -4,19 +4,35 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontFamily
 import com.github.bkmbigo.solitaireanimation.presentation.locals.cardtheme.DefaultCardTheme
 import com.github.bkmbigo.solitaireanimation.presentation.locals.cardtheme.LocalCardTheme
+import com.github.bkmbigo.solitaireanimation.presentation.locals.cardtheme.rememberCardTheme
 import com.github.bkmbigo.solitaireanimation.presentation.locals.resourceprovider.DefaultResourceProvider
 import com.github.bkmbigo.solitaireanimation.presentation.locals.resourceprovider.LocalResourceProvider
+import com.github.bkmbigo.solitaireanimation.presentation.utils.fonts.localfonts.LocalFontFamily
+import com.github.bkmbigo.solitaireanimation.presentation.utils.fonts.provideFontFamily
 
 @Composable
 fun SolitaireTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable ()-> Unit
 ) {
+    var andikaFont by remember { mutableStateOf<FontFamily?>(null) }
+    var lobsterTwoFont by remember { mutableStateOf<FontFamily?>(null) }
+
+    LaunchedEffect(Unit) {
+        andikaFont = LocalFontFamily.ANDIKA.provideFontFamily()
+        lobsterTwoFont = LocalFontFamily.LOBSTER_TWO.provideFontFamily()
+    }
 
     CompositionLocalProvider(
-        LocalCardTheme provides DefaultCardTheme,
+        LocalCardTheme provides rememberCardTheme(false, andikaFont, lobsterTwoFont),
         LocalResourceProvider provides DefaultResourceProvider
     ) {
         MaterialTheme(
