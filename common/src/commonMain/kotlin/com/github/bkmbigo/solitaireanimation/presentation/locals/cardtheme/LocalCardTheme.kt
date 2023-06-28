685a299a0e84f1f1a9931faa007d66397bcb6143
bkmbigo <bkmbigo@gmail.com>
2023-06-28 17:08:36 +0300

    add: Added cardSize to CardTheme

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/locals/cardtheme/LocalCardTheme.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/locals/cardtheme/LocalCardTheme.kt
@@ -5,17 +5,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 
 val LocalCardTheme = staticCompositionLocalOf<CardTheme> { DefaultCardTheme }
 
 val DefaultCardTheme = object: CardTheme {
     override val isDark: Boolean = false
     override val useMiniCards: Boolean = false
+
     override val cardFrontBackground: Color = Color.White
     override val cardBackBackground: Color = Color(0xFF552200)
     override val gameBackground: Color = Color(0xFF15360F)
+
     override val miniCardFont: FontFamily = FontFamily.Monospace
     override val appFont: FontFamily = FontFamily.SansSerif
+
+    override val cardSize: DpSize = DpSize((121.5).dp, (77.5).dp)
+    override val verticalCardStackSeparation: Dp = 20.dp
+    override val horizontalCardStackSeparation: Dp = 20.dp
 }
 @Composable
 fun rememberCardTheme(
@@ -31,5 +40,8 @@ fun rememberCardTheme(
         override val gameBackground: Color = Color(0xFF15360F)
         override val miniCardFont: FontFamily = lobsterTwoFamily ?: FontFamily.Monospace
         override val appFont: FontFamily = andikaFont ?: FontFamily.SansSerif
+        override val cardSize: DpSize = DpSize((121.5).dp, (77.5).dp)
+        override val verticalCardStackSeparation: Dp = 20.dp
+        override val horizontalCardStackSeparation: Dp = 20.dp
     }
 }
