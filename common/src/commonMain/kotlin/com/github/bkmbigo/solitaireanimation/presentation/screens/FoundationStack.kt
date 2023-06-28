b98e5be779908457c2770d078c32ebe245fb1300
bkmbigo <bkmbigo@gmail.com>
2023-06-28 20:51:24 +0300

    add: Created typesafe FoundationStack enum class

---
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/FoundationStack.kt
@@ -0,0 +1,9 @@
+package com.github.bkmbigo.solitaireanimation.presentation.screens
+
+/**A typesafe collection of Solitaire Foundation Stacks*/
+enum class FoundationStack {
+    SPADE,
+    CLOVER,
+    HEARTS,
+    DIAMOND
+}
