70c44dc105aa909930f21c89b0dd3fc8657cb51f
bkmbigo <bkmbigo@gmail.com>
2023-06-28 20:51:14 +0300

    add: Created typesafe TableStack enum class

---
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/TableStack.kt
@@ -0,0 +1,14 @@
+package com.github.bkmbigo.solitaireanimation.presentation.screens
+
+/**
+ * A typesafe collection of available stacks
+ */
+enum class TableStack {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    SIXTH,
+    SEVENTH
+}
