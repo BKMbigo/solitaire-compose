fc177611f48643f747cf0c83a957ae79c28f2fb4
bkmbigo <bkmbigo@gmail.com>
2023-06-28 15:42:14 +0300

    refactor: Changed the enumerations of GameState

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/GameState.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/GameState.kt
@@ -1,7 +1,19 @@
 package com.github.bkmbigo.solitaireanimation.presentation.screens
 
+/**
+ * State of the Solitaire Game
+ */
 enum class GameState {
-    LOADING,
+    /**The game is new and the user has not yet initiated the game.*/
+    NEW,
+    /**The game is playing the entry animation of the game*/
+    ENTRY_ANIMATION,
+    /**The user is currently playing the game*/
     PLAYING,
-    PAUSED
+    /**The user has paused the current game*/
+    PAUSED,
+    /**The game is currently playing the exit animation*/
+    EXIT_ANIMATION,
+    /**The user has completed the game. It can occur when the user has won a game or when the game cannot end*/
+    FINISHED_GAME
 }
