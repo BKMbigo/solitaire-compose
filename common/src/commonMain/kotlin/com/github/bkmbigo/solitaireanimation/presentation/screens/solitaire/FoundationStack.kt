110a2e40b31ef4a220be743eef00ada8f48904fd
bkmbigo <bkmbigo@gmail.com>
2023-06-29 14:26:52 +0300

    add: Added CardSuite to FoundationStack

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/solitaire/FoundationStack.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaireanimation/presentation/screens/solitaire/FoundationStack.kt
@@ -1,9 +1,11 @@
 package com.github.bkmbigo.solitaireanimation.presentation.screens.solitaire
 
+import com.github.bkmbigo.solitaireanimation.models.CardSuite
+
 /**A typesafe collection of Solitaire Foundation Stacks*/
-enum class FoundationStack {
-    SPADE,
-    CLOVER,
-    HEARTS,
-    DIAMOND
+enum class FoundationStack(val suite: CardSuite) {
+    SPADE(CardSuite.SPADE),
+    CLOVER(CardSuite.CLOVER),
+    HEARTS(CardSuite.HEARTS),
+    DIAMOND(CardSuite.DIAMOND)
 }
