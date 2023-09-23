f4543381de94db0d53011cfcb607e9e689293a2a
bkmbigo <bkmbigo@gmail.com>
2023-09-23 12:40:31 +0300

    add: Add hint parameter to SolitaireState
    [minor changes]
    
    [notes]

--- common/src/commonMain/kotlin/com/github/bkmbigo/solitaire/presentation/ui/game/card/solitaire/screens/SolitaireState.kt
+++ common/src/commonMain/kotlin/com/github/bkmbigo/solitaire/presentation/ui/game/card/solitaire/screens/SolitaireState.kt
@@ -1,9 +1,11 @@
 package com.github.bkmbigo.solitaire.presentation.ui.game.card.solitaire.screens
 
 import com.github.bkmbigo.solitaire.game.solitaire.SolitaireGame
+import com.github.bkmbigo.solitaire.game.solitaire.moves.SolitaireUserMove
 
 data class SolitaireState(
     val game: SolitaireGame = SolitaireGame.EMPTY_GAME,
+    val hint: SolitaireUserMove? = null,
     val canUndo: Boolean = false,
     val canRedo: Boolean = false,
     val isWon: Boolean = false,
