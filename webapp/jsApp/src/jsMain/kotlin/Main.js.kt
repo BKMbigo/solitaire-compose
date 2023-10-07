4fa877688616a697cf878aa414e304a3406f64ce
bkmbigo <bkmbigo@gmail.com>
2023-10-07 18:44:40 +0300

    fix: added production resource paths

--- webapp/jsApp/src/jsMain/kotlin/Main.js.kt
+++ webapp/jsApp/src/jsMain/kotlin/Main.js.kt
@@ -4,9 +4,14 @@ import com.github.bkmbigo.solitaire.utils.Platform
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
+
+    val generalResourcePath = "solitaire-compose/" // Use in production
+//    val generalResourcePath = "" // Use in local development
+
     onWasmReady {
         BrowserViewportWindow("Solitaire") {
             SolitaireTheme(
+                generalResourcePath = generalResourcePath,
                 platform = Platform.JS
             ) {
                 StartScreen()
--- webapp/wasmApp/src/wasmJsMain/kotlin/Main.wasm.kt
+++ webapp/wasmApp/src/wasmJsMain/kotlin/Main.wasm.kt
@@ -6,8 +6,13 @@ import com.github.bkmbigo.solitaire.utils.Platform
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+
+    val generalResourcePath = "solitaire-compose/" // Use in production
+//    val generalResourcePath = "" // Use in local development
+
     CanvasBasedWindow("Solitaire") {
         SolitaireTheme(
+            generalResourcePath = generalResourcePath,
             platform = Platform.WASM
         ) {
             StartScreen()
